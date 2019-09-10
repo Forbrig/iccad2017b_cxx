4e727c93d83a81d8c9723967bc454a85e0ade092
Vitor Forbrig <vitorforbrig@gmail.com>
2019-09-09 22:32:50 -0300

    trying rapidcheck to run tests on r tree

--- test/test_rtree.cxx
+++ test/test_rtree.cxx
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <fstream>
 #include <sstream>
+#include <rapidcheck.h>
 
 #include <RTree.h>
 
@@ -15,60 +16,86 @@ using namespace std::chrono;
 using namespace iccad;
 
 bool MySearchCallback(int id) {
-    cout << "Hit data rect " << id << "\n";
+    // cout << "Hit data rect " << id << "\n";
     return true; // keep going
 }
 
-int main(int argc, char ** argv) {
-    Input i;
-    parser::parse_file(i, argv[1]);
-    ofstream fout(argv[2]);
+// int main(int argc, char ** argv) {
+//     Input i;
+//     parser::parse_file(i, argv[1]);
+//     ofstream fout(argv[2]);
 
-    if (argc != 3) {
-        cout << "usage: " << argv[0] << " <testcase> <output_file_for_solution>\n";
-        return -1;
-    }
-    vector<Shape> shapes = get_routed_shapes(i);
-    vector<Shape> obstacles = get_obstacles(i);
+//     if (argc != 3) {
+//         cout << "usage: " << argv[0] << " <testcase> <output_file_for_solution>\n";
+//         return -1;
+//     }
+//     vector<Shape> shapes = get_routed_shapes(i);
+//     vector<Shape> obstacles = get_obstacles(i);
 
-	typedef RTree<int, int, 3, float> MyTree;
+// 	typedef RTree<int, int, 3, float> MyTree;
+//     MyTree tree;
+
+//     unsigned int j;
+//     cout << "number of rectangles is " << shapes.size() + obstacles.size() << "\n";
+
+//     auto t1 = std::chrono::high_resolution_clock::now();
+
+//     for (j = 0; j < shapes.size(); j++) {
+//         int low[] = {shapes[j].a.x, shapes[j].a.y, shapes[j].a.z};
+//         int high[] = {shapes[j].b.x, shapes[j].b.y, shapes[j].b.z};
+
+//         tree.Insert(low, high, j); // Note, all values including zero are fine in this version
+//     }
+
+//     auto t2 = std::chrono::high_resolution_clock::now();
+//     cout << "the duration was " << duration_cast<microseconds>( t2 - t1 ).count() << "\n";
+
+
+//     unsigned int nhits;
+    
+    
+//     int low[] = {shapes[j].a.x, shapes[j].a.y, shapes[j].a.z};
+//     int high[] = {shapes[j].b.x, shapes[j].b.y, shapes[j].b.z};
+    
+//     int search_low[] = {63445,114074, 0};
+//     int search_high[] = {63466,115543, 0};
+//     nhits = tree.Search(search_low, search_high, MySearchCallback);
+
+//     // int search_rect(5463, 3699, 0, 5467, 3714, 0);
+//     // nhits = tree.Search(search_rect.min, search_rect.max, MySearchCallback);
+
+//     // Rect search_rect2(904, 415, 0, 908, 458, 0);
+//     // nhits = tree.Search(search_rect2.min, search_rect2.max, MySearchCallback);
+
+//     // auto search_low = {3465, 3332, 0};
+//     // auto search_hight = {3497, 3348, 0};
+//     // tree.Search(search_low, search_hight, MySearchCallback);
+    
+//     return 0;
+// }
+
+
+void test_query(std::vector<int[3]> &low, std::vector<int[3]> &high) {
+    typedef RTree<int, int, 3, float> MyTree;
     MyTree tree;
 
-    unsigned int j;
-    cout << "number of rectangles is " << shapes.size() + obstacles.size() << "\n";
-
-    auto t1 = std::chrono::high_resolution_clock::now();
-
-    for (j = 0; j < shapes.size(); j++) {
-        int low[] = {shapes[j].a.x, shapes[j].a.y, shapes[j].a.z};
-        int high[] = {shapes[j].b.x, shapes[j].b.y, shapes[j].b.z};
-
-        tree.Insert(low, high, j); // Note, all values including zero are fine in this version
+    // insert on tree
+    for (size_t i = 0; i < low.size(); i++) {
+        tree.Insert(low[i], high[i], i);
     }
 
-    auto t2 = std::chrono::high_resolution_clock::now();
-    cout << "the duration was " << duration_cast<microseconds>( t2 - t1 ).count() << "\n";
+    // find the shapes
 
+    for (size_t i = 0; i < low.size(); i++) {
+        int found = tree.Search(low[i], high[i], MySearchCallback);
+        RC_ASSERT(found > 0);
+    }
+}
 
-    unsigned int nhits;
-    
-    
-    int low[] = {shapes[j].a.x, shapes[j].a.y, shapes[j].a.z};
-    int high[] = {shapes[j].b.x, shapes[j].b.y, shapes[j].b.z};
-    
-    int search_low[] = {63445,114074, 0};
-    int search_high[] = {63466,115543, 0};
-    nhits = tree.Search(search_low, search_high, MySearchCallback);
+int main(int argc, char ** argv) {
+    rc::check("Check that added shapes can be queried", [](std::vector<int[3]> &low, std::vector<int[3]> &high) {
+        test_query(low, high);
+    });
 
-    // int search_rect(5463, 3699, 0, 5467, 3714, 0);
-    // nhits = tree.Search(search_rect.min, search_rect.max, MySearchCallback);
-
-    // Rect search_rect2(904, 415, 0, 908, 458, 0);
-    // nhits = tree.Search(search_rect2.min, search_rect2.max, MySearchCallback);
-
-    // auto search_low = {3465, 3332, 0};
-    // auto search_hight = {3497, 3348, 0};
-    // tree.Search(search_low, search_hight, MySearchCallback);
-    
     return 0;
 }
