1fbfc04c1c5e7ae9ab92f9d05425123c3f720097
Fobrig <vitorforbrig@gmail.com>
2019-10-11 22:10:08 -0300

    crying in polish test 4

--- test/test_rtree_integration.cxx
+++ test/test_rtree_integration.cxx
@@ -18,6 +18,10 @@ using namespace std;
 using namespace std::chrono;
 using namespace iccad;
 
+#define MAXN 100009
+
+typedef pair<int, int> ii;
+
 // id do shape encotrado, vetor dos shapes, vetor dos encontrados (bool), contador
 bool search_n_callback(int id, vector<bool> &found, int &n) {  
     if (found[id] == false) {
@@ -28,10 +32,8 @@ bool search_n_callback(int id, vector<bool> &found, int &n) {
 }
 
 // coloca em uma fila o id do item encontrado e a distancia
-bool search_neighboors(int id, Shape ref, const vector<Shape> &shapes, set<pair<int, int>> &neighboors) {
-    int found_dist = distance(ref, shapes[id]);
-    neighboors.insert({id, found_dist});
-    
+bool search_neighboors(int id, Shape ref, const vector<Shape> &shapes, set<ii> &neighboors) {
+    neighboors.insert(ii(distance(ref, shapes[id]), id));
     return true;
 }
 
@@ -187,6 +189,21 @@ void test_rtree_collect_neg(const Shape s, const vector<Shape> shapes) {
 }
 
 void test_rtree_closest_n_shapes(const Shape s, const vector<Shape> shapes, unsigned int p) {
+    // cout << x1 << "\n";
+    // cout << y1 << "\n";
+    // cout << z1 << "\n";
+    // cout << x2 << "\n";
+    // cout << y2 << "\n";
+    // cout << z2 << "\n";
+    
+    // cout << s.a.x << "\n";
+    // cout << s.a.y << "\n";
+    // cout << s.a.z << "\n";
+    // cout << s.b.x << "\n";
+    // cout << s.b.y << "\n";
+    // cout << s.b.z << "\n";
+
+    // cin.get();
     
     if (shapes.size() == 0) {
         RC_DISCARD("discarding empty testcase");
@@ -203,29 +220,86 @@ void test_rtree_closest_n_shapes(const Shape s, const vector<Shape> shapes, unsi
 
     // insert on tree
     for (int j = 0; j < shapes.size(); j++) {
-        int low[] = {shapes[j].a.x, shapes[j].a.y, 0};
-        int high[] = {shapes[j].b.x, shapes[j].b.y, 0};
+        int low[] = {shapes[j].a.x, shapes[j].a.y, shapes[j].a.z};
+        int high[] = {shapes[j].b.x, shapes[j].b.y, shapes[j].b.z};
 
         tree.Insert(low, high, j);
     }
 
     // search nearests
     int ns = 0;
-    // vector<bool> found(shapes.size(), false); // salva os shapes que colidem
+    vector<bool> found(shapes.size(), false); // salva os shapes que colidem
 
     // fila de prioridades com as shapes mais próximas
-    set <pair<int, int>> neighboors;
+    set<ii> neighboors;
 
-    {
-        // primeiro procura shapes nas dimensões do s
-        rtree_search_shape(tree, s, [&s, &shapes, &neighboors](int id)->bool { return search_neighboors(id, s, shapes, neighboors); } );
-    }
+    // primeiro procura shapes nas dimensões do s
+    rtree_search_shape(tree, s, [&s, &shapes, &neighboors](int id)->bool { return search_neighboors(id, s, shapes, neighboors); } );
 
-    for (int i = 1; ns < n && i < 123456789; i = i * 2) { // para quando encontrar a quantidade de retângulos n
+    Shape z = s;
 
-        vector<Shape> direcs = directions(s, i);
+    // z.a.x = 4;
+    // z.a.y = 4;
+    // z.a.z = 4;
+    // z.b.x = 8;
+    // z.b.y = 8;
+    // z.b.z = 8;
+    
+    // omitindo z
+    // cout << "Polygon((" << z.a.x << "," << z.a.y << "),";
+    // cout << "(" << z.a.x << "," << z.b.y << "),";
+    // cout << "(" << z.b.x << "," << z.b.y << "),";
+    // cout << "(" << z.b.x << "," << z.a.y << "))\n";
+    // cin.get();
+
+    // omitindo x
+    // cout << "Polygon((" << z.a.z << "," << z.a.y << "),";
+    // cout << "(" << z.a.z << "," << z.b.y << "),";
+    // cout << "(" << z.b.z << "," << z.b.y << "),";
+    // cout << "(" << z.b.z << "," << z.a.y << "))\n";
+    // cin.get();
+
+    // // omitindo y
+    // cout << "Polygon((" << z.a.x << "," << z.a.z << "),";
+    // cout << "(" << z.a.x << "," << z.b.z << "),";
+    // cout << "(" << z.b.x << "," << z.b.z << "),";
+    // cout << "(" << z.b.x << "," << z.a.z << "))\n";
+    // cin.get();
+
+    int i = 0;
+    for (i = 1; ns < n && i < 123456; i = i * 2) { // para quando encontrar a quantidade de retângulos n
+
+        vector<Shape> direcs = directions(z, i);
 
         for (int side = 0; side < 6; side++) {
+            //sem a coordenada z
+            // cout << "Polygon((" << direcs[side].a.x << "," << direcs[side].a.y << "),";
+            // cout << "(" << direcs[side].a.x << "," << direcs[side].b.y << "),";
+            // cout << "(" << direcs[side].b.x << "," << direcs[side].b.y << "),";
+            // cout << "(" << direcs[side].b.x << "," << direcs[side].a.y << "))\n";
+            // cin.get();
+
+            //sem a coordenada x
+            // cout << "Polygon((" << direcs[side].a.z << "," << direcs[side].a.y << "),";
+            // cout << "(" << direcs[side].a.z << "," << direcs[side].b.y << "),";
+            // cout << "(" << direcs[side].b.z << "," << direcs[side].b.y << "),";
+            // cout << "(" << direcs[side].b.z << "," << direcs[side].a.y << "))\n";
+            // cin.get();
+
+            // //sem a coordenada y
+            // cout << "Polygon((" << direcs[side].a.x << "," << direcs[side].a.z << "),";
+            // cout << "(" << direcs[side].a.x << "," << direcs[side].b.z << "),";
+            // cout << "(" << direcs[side].b.x << "," << direcs[side].b.z << "),";
+            // cout << "(" << direcs[side].b.x << "," << direcs[side].a.z << "))\n";
+            // cin.get();
+
+            // cout << direcs[side].a.x << "\n";
+            // cout << direcs[side].a.y << "\n";
+            // cout << direcs[side].a.z << "\n";
+            // cout << direcs[side].b.x << "\n";
+            // cout << direcs[side].b.y << "\n";
+            // cout << direcs[side].b.z << "\n";
+
             rtree_search_shape(tree, direcs[side], [&s, &shapes, &neighboors](int id)->bool { return search_neighboors(id, s, shapes, neighboors); } );
         }
 
@@ -237,20 +311,49 @@ void test_rtree_closest_n_shapes(const Shape s, const vector<Shape> shapes, unsi
 
         while (!neighboors.empty()) {
             int dist = neighboors.begin()->first;
-            if (dist <= i) { // se a distancia for menor que a area procurada entao retira a shape do set
+            int id = neighboors.begin()->second;
+            if (dist < i) { // se a distancia for menor que a expansao procurada entao retira a shape do set
+                if (found[id] == false) {
+                    found[id] = true;
+                    ns++; // contabiliza nos shapes encontrados
+                }
                 neighboors.erase(neighboors.begin());
-                ns++; // contabliza nos shapes encotrados
             } else { // se for maior para pois o set esta ordenado pela distancia
                 break;
             }
         }
         
-        s.expand(i);
+        z = s.expand(i);
     }
 
     // testa se os elementos que sobraram estao realmente mais distantes que i
+    // for (int k = 0; k < shapes.size(); k++) {
+    //     if ((found[k] == false) && (distance(s, shapes[k]) < i)) {
+            // if (distance(s, shapes[k]) < i) {
+    //             cout << "erro\n";
+                
+    //         //     cout << "current dist: " << i << " found dist: "<< found_dist << "\n";
 
-    cout << n << " " << ns << "\n";
+    //         //     cout << s.a.x << " ";
+    //         //     cout << s.a.y << " ";
+    //         //     cout << s.a.z << "\n";
+    //         //     cout << s.b.x << " ";
+    //         //     cout << s.b.y << " ";
+    //         //     cout << s.b.z << "\n";
+
+    //         //     cout << shapes[k].a.x << " ";
+    //         //     cout << shapes[k].a.y << " ";
+    //         //     cout << shapes[k].a.z << "\n";
+    //         //     cout << shapes[k].b.x << " ";
+    //         //     cout << shapes[k].b.y << " ";
+    //         //     cout << shapes[k].b.z << "\n";
+                // RC_ASSERT(false);
+            // }
+    //     }
+    // }
+
+    // cout << n << " " << ns << "\n";
+    // cout << "new\n";
 }
 
 int main(int argc, char ** argv) {
